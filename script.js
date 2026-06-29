f24178a8f1558d222457de09705a424505633a5d
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-06-30 01:08:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const header = document.querySelector("header");
 
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 40){
+
+        header.classList.add("scrolled");
+
+    }
+
+    else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
